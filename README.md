0af7eb08066eae8d989124ae3977634129076ae6
Jay Shah <shahj097@gmail.com>
2023-10-07 23:41:23 +0530

    updated algorithm to be NISQ Era friendly

--- README.md
+++ README.md
@@ -4,6 +4,12 @@
 
 ## Approach
 
+0. Break the given list into a list of lists with the size of each list being 4. Pad the lists with 1 when required. This is to make the algorithm NISQ era friendly.
+
+```For example, a given list [a,-b,c,d,e] changes to [[a,-b,c,d],[e,1,1,1]]```
+
+FOR EACH LIST
+
 1. Using simple element wise self division, transform a given list [a,-b,c,d] to [1,-1,1,1]
 
 2. Create a diagonal matrix U of size (len(list) X len(list)) with diagonal entries as [1,-1,1,1]
@@ -16,8 +22,16 @@
 
 6. If a phase of 3.142 is not found (all phases are 0), it corresponds to all positive numbers. The algorithm returns `FALSE`
 
+## Advantages
+
+1. The Algorithm is NISQ Era friendly since it requires only 3 qubits to run the algorithm.
+
+2. The algorithm will successfully solve the challenge irrespective of the magnitude of the positive/negative number due to the use of self division.
+
+3. The Algorithm uses the popular Iterative Quantum Phase Estimation technique at its core to solve the problem.
+
 ## Limitations
 
-1. A given list must be padded with Ones to the next power of 2.
+1. A given list must be padded with Ones to the next power of 2 when required.
 
-2. Although the coded algorithm correctly identifies the case when there are all positive numbers and successfully solves the problem at hand, in the case of multiple negative values it messes up the index for a phase value.
+2. The algorithm correctly differentiates the case of all positive numbers versus a case where list contains negative values and successfully solves the problem at hand. However, in the case of multiple negative values it messes up the index of the phase value.
--- find_negative_num/main.py
+++ find_negative_num/main.py
@@ -3,35 +3,9 @@ import numpy as np
 
 from iqpe import IQPE
 
-# list1 = [-1,2,3,4]
-
-# def get_oracle_matrix(list_nums:list):
-#     diagonal_values = []
-#     for num in list_nums:
-#         try: 
-#             value = num//num
-#         except ZeroDivisionError:
-#             value = 1
-    
-# U = np.array([[1,0,0,0],[0,1,0,0],[0,0,-1,0],[0,0,0,-1]])
-# # eval, evec = np.linalg.eig(U)
-# # print(len(U.tolist()))
-# # print(evec)
-
-# # declare the eigenstate (|psi>)
-# ket_psi = np.array([0,1])
-
-# # Unitary Gate (U)
-# unitary = np.array([[1,0],[0,np.exp(1j*(np.pi))]]) # np.exp(1j*(2 * np.pi * 0.0053))
-
-# # bit accuracy
-# m = 4
-
-# get a Iterative_QPE class instance
-# test_case = IQPE(unitary=unitary,ket_psi=ket_psi,precision_bits=m,SHOTS=100)
-# test_case.solve()
-# k = np.kron(ket_psi,[1/np.sqrt(2),1/np.sqrt(2)])
-# print(k)
+def break_list(list_:list,size:int = 4):
+    for i in range(0,len(list_),size):
+        yield list_[i:i+size]
 
 def get_unitary(nums_list):
     diagonal_values = []
@@ -44,25 +18,38 @@ def get_unitary(nums_list):
     unitary = np.eye(len(nums_list))
     for i in range(len(diagonal_values)):
         unitary[i,i] = diagonal_values[i]
-    # print(unitary,diagonal_values)
     return unitary, diagonal_values
 
 def find_negative_numbers(nums_list:list = [4,1,4,1], precision_bits:int = 4, SHOTS:int = 1024):
     unitary, diagonal_values = get_unitary(nums_list=nums_list)
-    ev, evec = np.linalg.eig(unitary)
-    # print(ev, evec)
     for j in range(len(diagonal_values)):
         ket_psi = [0] * len(diagonal_values)
         ket_psi[j] = 1
         iqpe = IQPE(unitary=unitary,ket_psi=np.array(ket_psi),precision_bits=precision_bits, SHOTS=SHOTS)
         theta_binary, theta, theta_frac, phi = iqpe.solve()
-        if np.round(phi) == np.pi:
+        if  phi==np.pi:
             return True
+        
     return False
+
+def main(my_list:list):
+    my_lists = list(break_list(my_list,4))
+    if len(my_lists[-1])!=4:
+        for j in range(len(my_lists[-1]),4):
+            my_lists[-1].append(1)
+
+    for my_list in my_lists:
+        result = find_negative_numbers(my_list)
+        if result==True:
+            print('There exists at least one negative entry!')
+            return True
+    
+    if result==False:
+        print('All numbers are positive!')
+        return False
     
 if __name__=='__main__':
-    result = find_negative_numbers()
-    if result==True:
-        print('There exists at least one negative entry!')
-    else:
-        print('All numbers are positive!')
+    my_list = [-1,2,3,4,5]
+    res = main(my_list=my_list)
+    print(f'This is the required output -> {res}')
+
